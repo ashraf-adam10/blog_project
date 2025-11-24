1d9b87e17624b1d5e4f82ad5efd26abe06e0d00a
ashraf-adam10 <ashrafadam4k@gmail.com>
2025-11-24 19:22:28 +0200

    edit - 2

--- blog_app/main.js
+++ blog_app/main.js
@@ -2,11 +2,25 @@ let textBox = document.querySelector(".textBox");
 let imgBox = document.querySelector(".imgBox");
 let input = document.getElementById("file-input");
 let preview = document.getElementById("preview");
+let postInput = document.getElementById("postInput");
+let textInput = document.getElementById("textInput");
+let addPostBtn = document.getElementById("addPostBtn");
+let mainTitle = document.getElementById("mainTitle");
+let postImg = document.getElementById("postImg");
 
 
-imgBox.onclick = function(){
+addPostBtn.addEventListener("click", ()=> {
+    mainTitle.innerHTML = textInput.value;
+    postImg.src = preview.src;
+    textBox.innerHTML = postInput.value;
+    textInput.value = "";
+    preview.src = "";
+    postInput.value = "";
+});
+
+imgBox.addEventListener("click", ()=> {
     textBox.classList.toggle('hide');
-}
+});
 
 input.addEventListener("change", () => {
     let file = input.files[0];
