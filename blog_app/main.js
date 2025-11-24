665847257d677edbb7f727ceecf1d3a8f608ec04
ashraf-adam10 <ashrafadam4k@gmail.com>
2025-11-24 11:51:45 +0200

    edit - 1

--- blog_app/main.js
+++ blog_app/main.js
@@ -1,6 +1,14 @@
 let textBox = document.querySelector(".textBox");
 let imgBox = document.querySelector(".imgBox");
+let input = document.getElementById("file-input");
+let preview = document.getElementById("preview");
+
 
 imgBox.onclick = function(){
     textBox.classList.toggle('hide');
 }
+
+input.addEventListener("change", () => {
+    let file = input.files[0];
+    preview.src = URL.createObjectURL(file);
+});
